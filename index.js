c2639bf4014b761e96e9249361fa35e03d769252
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-04 02:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,8 @@ function appendKitten(name){
   var newKittensArray = [...kittens,name]
   return newKittensArray
 }
+
+function prependKitten(name){
+  var newKittensArray = [name,...kittens]
+  return newKittensArray
+}
